f9ef091e1fd98431ac995dc6ab2d55b694239afb
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-12 15:10:34 +0100

    Created the project files.

---
+++ src/Serilog.Enrichers.HttpContextInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Serilog.Enrichers.HttpContextInfo")]
+[assembly: AssemblyDescription("Enrich Serilog log events with properties from the current HttpContext.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Steven Atkinson")]
+[assembly: AssemblyProduct("Serilog.Enrichers.HttpContextInfo")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f6af92e-1d10-46d7-bc79-59d3161526c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

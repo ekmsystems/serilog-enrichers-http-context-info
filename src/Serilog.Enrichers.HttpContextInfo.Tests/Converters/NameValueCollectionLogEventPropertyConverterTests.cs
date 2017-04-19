a4ce728a870b1cbc3d3ab9a33a88c283a75ef87d
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-19 15:15:21 +0100

    Added a converter for converting NameValueCollection into a collection of LogEventProperty.

---
+++ src/Serilog.Enrichers.HttpContextInfo.Tests/Converters/NameValueCollectionLogEventPropertyConverterTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Serilog.Converters;
+using Serilog.Core;
+
+namespace Serilog.Tests.Converters
+{
+    [TestFixture]
+    [Parallelizable]
+    public class NameValueCollectionLogEventPropertyConverterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _propertyFactory = new Mock<ILogEventPropertyFactory>();
+            _converter = new NameValueCollectionLogEventPropertyConverter(_propertyFactory.Object, PropertyName);
+        }
+
+        private const string PropertyName = "MyCollection";
+        private Mock<ILogEventPropertyFactory> _propertyFactory;
+        private ILogEventPropertyConverter<NameValueCollection> _converter;
+
+        [Test]
+        public void Convert_ShouldReturn_LogEventProperties()
+        {
+            var collection = new NameValueCollection
+            {
+                {"Test", "value"}
+            };
+
+            var results = _converter.Convert(collection).ToArray();
+
+            Assert.AreEqual(1, results.Length);
+
+            _propertyFactory.Verify(x => x.CreateProperty($"{PropertyName}[Test]", "value", false), Times.Once);
+        }
+    }
+}
---
+++ src/Serilog.Enrichers.HttpContextInfo/Converters/NameValueCollectionLogEventPropertyConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Converters
+{
+    internal class NameValueCollectionLogEventPropertyConverter : ILogEventPropertyConverter<NameValueCollection>
+    {
+        private readonly ILogEventPropertyFactory _propertyFactory;
+        private readonly string _propertyName;
+
+        public NameValueCollectionLogEventPropertyConverter(
+            ILogEventPropertyFactory propertyFactory,
+            string propertyName)
+        {
+            _propertyFactory = propertyFactory;
+            _propertyName = propertyName;
+        }
+
+        public IEnumerable<LogEventProperty> Convert(NameValueCollection obj)
+        {
+            return obj?.AllKeys
+                       .Select(key => _propertyFactory.CreateProperty($"{_propertyName}[{key}]", obj[key]))
+                   ?? Enumerable.Empty<LogEventProperty>();
+        }
+    }
+}

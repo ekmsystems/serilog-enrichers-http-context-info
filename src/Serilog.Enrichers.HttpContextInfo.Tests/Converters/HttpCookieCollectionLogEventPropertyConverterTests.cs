5fa0f1ab7c5d14668a219821e41012f4d81f0742
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-19 15:56:17 +0100

    Added the HttpCookieCollectionLogEventPropertyConverter with tests.

---
+++ src/Serilog.Enrichers.HttpContextInfo.Tests/Converters/HttpCookieCollectionLogEventPropertyConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using Moq;
+using NUnit.Framework;
+using Serilog.Converters;
+using Serilog.Core;
+
+namespace Serilog.Tests.Converters
+{
+    [TestFixture]
+    [Parallelizable]
+    public class HttpCookieCollectionLogEventPropertyConverterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _propertyFactory = new Mock<ILogEventPropertyFactory>();
+            _converter = new HttpCookieCollectionLogEventPropertyConverter(_propertyFactory.Object, PropertyName);
+        }
+
+        private const string PropertyName = "MyCollection";
+        private Mock<ILogEventPropertyFactory> _propertyFactory;
+        private ILogEventPropertyConverter<HttpCookieCollection> _converter;
+
+        [Test]
+        public void Convert_ShouldReturn_LogEventProperties()
+        {
+            var expires = new DateTime(2017, 1, 1);
+            var collection = new HttpCookieCollection
+            {
+                new HttpCookie("Test", "value") {Domain = "localhost", Expires = expires, Path = "/"}
+            };
+
+            var results = _converter.Convert(collection).ToArray();
+
+            Assert.AreEqual(5, results.Length);
+
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[Test].Name", "Test", false), Times.Once);
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[Test].Value", "value", false), Times.Once);
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[Test].Domain", "localhost", false), Times.Once);
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[Test].Expires", expires.ToString("u"), false), Times.Once);
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[Test].Path", "/", false), Times.Once);
+        }
+    }
+}
---
+++ src/Serilog.Enrichers.HttpContextInfo/Converters/HttpCookieCollectionLogEventPropertyConverter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Converters
+{
+    internal class HttpCookieCollectionLogEventPropertyConverter : ILogEventPropertyConverter<HttpCookieCollection>
+    {
+        private readonly ILogEventPropertyFactory _propertyFactory;
+        private readonly string _propertyName;
+
+        public HttpCookieCollectionLogEventPropertyConverter(
+            ILogEventPropertyFactory propertyFactory,
+            string propertyName)
+        {
+            _propertyFactory = propertyFactory;
+            _propertyName = propertyName;
+        }
+
+        public IEnumerable<LogEventProperty> Convert(HttpCookieCollection obj)
+        {
+            return obj?.AllKeys
+                       .SelectMany(key => new[]
+                       {
+                           _propertyFactory.CreateProperty(
+                               $"{_propertyName}[{key}].Name",
+                               obj[key].Name),
+                           _propertyFactory.CreateProperty(
+                               $"{_propertyName}[{key}].Value",
+                               obj[key].Value),
+                           _propertyFactory.CreateProperty(
+                               $"{_propertyName}[{key}].Domain",
+                               obj[key].Domain),
+                           _propertyFactory.CreateProperty(
+                               $"{_propertyName}[{key}].Expires",
+                               obj[key].Expires.ToString("u")),
+                           _propertyFactory.CreateProperty(
+                               $"{_propertyName}[{key}].Path",
+                               obj[key].Path)
+                       })
+                   ?? Enumerable.Empty<LogEventProperty>();
+        }
+    }
+}

51d475d19bbe79b664401ad6fd7e8226f340c669
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-20 09:20:25 +0100

    Added the HttpFileCollectionWrapperLogEventPropertyConverter with tests.

---
+++ src/Serilog.Enrichers.HttpContextInfo.Tests/Converters/HttpFileCollectionWrapperLogEventPropertyConverterTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Serilog.Converters;
+using Serilog.Core;
+
+namespace Serilog.Tests.Converters
+{
+    [TestFixture]
+    [Parallelizable]
+    public class HttpFileCollectionWrapperLogEventPropertyConverterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _propertyFactory = new Mock<ILogEventPropertyFactory>();
+            _converter = new HttpFileCollectionWrapperLogEventPropertyConverter(_propertyFactory.Object, PropertyName);
+        }
+
+        private const string PropertyName = "MyCollection";
+        private Mock<ILogEventPropertyFactory> _propertyFactory;
+        private ILogEventPropertyConverter<IHttpFileCollectionWrapper> _converter;
+
+        [Test]
+        public void Convert_ShouldReturn_LogEventProperties()
+        {
+            var file = new Mock<IHttpPostedFileWrapper>();
+            var collection = new Mock<IHttpFileCollectionWrapper>();
+
+            file.SetupGet(x => x.FileName).Returns("test.png");
+            file.SetupGet(x => x.ContentLength).Returns(100);
+            file.SetupGet(x => x.ContentType).Returns("binary");
+            collection.Setup(x => x.AllKeys).Returns(new[] {file.Object.FileName});
+            collection.Setup(x => x.Get(file.Object.FileName)).Returns(file.Object);
+
+            var results = _converter.Convert(collection.Object).ToArray();
+
+            Assert.AreEqual(3, results.Length);
+
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[test.png].FileName", "test.png", false), Times.Once);
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[test.png].ContentLength", 100, false), Times.Once);
+            _propertyFactory
+                .Verify(x => x.CreateProperty($"{PropertyName}[test.png].ContentType", "binary", false), Times.Once);
+        }
+    }
+}
---
+++ src/Serilog.Enrichers.HttpContextInfo/Converters/HttpFileCollectionWrapperLogEventPropertyConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Converters
+{
+    internal class HttpFileCollectionWrapperLogEventPropertyConverter : ILogEventPropertyConverter<IHttpFileCollectionWrapper>
+    {
+        private readonly ILogEventPropertyFactory _propertyFactory;
+        private readonly string _propertyName;
+
+        public HttpFileCollectionWrapperLogEventPropertyConverter(
+            ILogEventPropertyFactory propertyFactory,
+            string propertyName)
+        {
+            _propertyFactory = propertyFactory;
+            _propertyName = propertyName;
+        }
+
+        public IEnumerable<LogEventProperty> Convert(IHttpFileCollectionWrapper obj)
+        {
+            return obj?.AllKeys.SelectMany(key => CreateProperty(key, obj.Get(key))) ??
+                   Enumerable.Empty<LogEventProperty>();
+        }
+
+        private IEnumerable<LogEventProperty> CreateProperty(string key, IHttpPostedFileWrapper file)
+        {
+            yield return _propertyFactory.CreateProperty($"{_propertyName}[{key}].FileName", file.FileName);
+            yield return _propertyFactory.CreateProperty($"{_propertyName}[{key}].ContentLength", file.ContentLength);
+            yield return _propertyFactory.CreateProperty($"{_propertyName}[{key}].ContentType", file.ContentType);
+        }
+    }
+}
